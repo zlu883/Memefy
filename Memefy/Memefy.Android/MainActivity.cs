a3cb27d370cac38ef3c33384314f3f17eef2b9bc
Jonny Lu <zlu883@aucklanduni.ac.nz>
2017-07-22 10:11:21 +1200

    First Commit

---
+++ Memefy/Memefy.Android/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Plugin.Permissions;
+
+namespace Memefy.Droid
+{
+	[Activity (Label = "Memefy", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar; 
+
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new Memefy.App ());
+		}
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
---
+++ Memefy/Memefy.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Memefy.Droid.MainApplication, Memefy.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5b00ff4e4408cf5a4a9f01ed693afc8b5.MainApplication.class, md5b00ff4e4408cf5a4a9f01ed693afc8b5.MainApplication.__md_methods);
+		
+	}
+}
